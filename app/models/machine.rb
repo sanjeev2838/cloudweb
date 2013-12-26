3cb5f385d5746c9d0896d1767972042df2cde677
sanjeevk <Sanjeevk.impinge@gmail.com>
2013-12-26 11:30:51 +0530

    Removed user refrence from machine and added bootloader version as parameter

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,7 +1,7 @@
 class Machine < ActiveRecord::Base
-  attr_accessible :activated_on, :firmware_version, :hw_config, :ip_address, :mac_address, :serial_number, :status
+  attr_accessible :activated_on, :firmware_version, :hw_config, :ip_address, :mac_address, :serial_number, :status,
+                  :bootloader_version
 
-  belongs_to :user
   #has_many :parent_profiles
   has_many :machine_logs
 end
