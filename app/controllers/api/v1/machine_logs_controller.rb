c1259284972c32e94a08ba8b0ad460ddbd846b33
sanjeevk <Sanjeevk.impinge@gmail.com>
2013-12-27 09:00:36 +0530

    Added api for machine_log

---
+++ app/controllers/api/v1/machine_logs_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::MachineLogsController < Api::V1::BaseController
+  #before_filter :authorize_admin!, :except => [:index, :show]
+  #before_filter :find_machine, :only => [:show, :update, :destroy]
+  #
+  #def index
+  #  @machines = MachineLog.all
+  #  respond_with(@machines)
+  #end
+  #
+  #def show
+  #  respond_with @machine
+  #end
+#  {"created_at":"2013-12-26T07:43:51Z","description":"testing ","id":1,"machine_id":null,"updated_at":"2013-12-26T07:43:51Z"}
+  def create
+    @machine = Machine.where(:serial_number => params[:serial_id]).first
+    machine_log = MachineLog.create(:machine_id => @machine.id, :description => params[:description])
+    if machine_log.valid?
+      render json:{:status => true}
+    else
+      render json:{:status => false, :message => "Unable to create machine_log on cloud"}
+    end
+  end
+
+  #def update
+  #  @machine.update_attributes(params[:machine])
+  #  respond_with(@machine)
+  #end
+  #
+  #def destroy
+  #  @machine.destroy
+  #  respond_with(@machine)
+  #end
+  #
+  #private
+  #
+  #def find_machine
+  #  @machine = Machine.find(params[:id])
+  #rescue ActiveRecord::RecordNotFound
+  #  error = { :error => "The machine you were looking for could not be found."}
+  #  respond_with(error, :status => 404)
+  #end
+
+end
--- app/models/machine_log.rb
+++ app/models/machine_log.rb
@@ -1,5 +1,5 @@
 class MachineLog < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description , :machine_id
 
   belongs_to :machine
 end
