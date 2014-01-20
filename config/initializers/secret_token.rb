ab7e0f8d2bbddb945e8034ce4413a57bf3cc52ae
Pardeep <pardeep@impingeonline.com>
2014-01-20 14:38:06 +0530

    Modify .gitignor file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudweb::Application.config.secret_token = '59b5402589b951e8617891cf2d6c599bb6d11d6cc2ea7f95b83de51858dac2bfcd52f6196e5b0d9c22f5a6fe5c175e190c1b09415aea7b10de2f31e44cc8b194'
