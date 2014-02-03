133d95a407568684b60ce9f2135f5575a50a4d90
sanjeevk <Sanjeevk.impinge@gmail.com>
2014-02-03 16:28:44 +0530

    refactor child_stat controller

--- app/controllers/api/v1/child_stats_controller.rb
+++ app/controllers/api/v1/child_stats_controller.rb
@@ -1,121 +1,75 @@
 class Api::V1::ChildStatsController < Api::V1::BaseController
-  before_filter :verify_token
 
-#todo remove parent_profile later on
-  before_filter :find_profile, :only => [:update,:index,:create]
+  before_filter :verify_token
+  before_filter :find_child_profile, :only => [:index, :create, :child_meals, :child_vaccines,:child_half_bottles ,:child_full_bottles]
 
   def index
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-    if params[:type].nil?
-      render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}
-      return
-    end
-    @stats = ChildStat.get_child_stat(@child_profile.id,@parent_profile.id,params[:type])
-    if @stats.empty?
-      render json:{:status => false, :message => "Child stats not found "}
-    end
+    render json:{:status => false, :message => 'qw'} if params[:type].nil?
+    @stats = ChildStat.get_child_stat(@child_profile.id,@profile.id,params[:type])
+    render json:{:status => false, :message => 'Child stats not found'}  if @stats.empty?
   end
 
-
-
   def child_vaccines
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-    if params[:type].nil?
-      render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}
-      return
-    end
+    render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}  if params[:type].nil?
     @vaccines = ChildStat.get_child_vaccine(@child_profile.id,@parent_profile.id,params[:type])
-    if @vaccines.empty?
-      render json:{:status => false, :message => "Child Vaccines not found "}
-    end
-
+    render json:{:status => false, :message => "Child Vaccines not found "}  if @vaccines.empty?
   end
 
   def child_meals
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-    if params[:type].nil?
-      render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}
-      return
-    end
-    @meals = ChildStat.get_child_meals(@child_profile.id,@parent_profile.id,params[:type])
-    if @meals.empty?
-      render json:{:status => false, :message => "Child meals not found "}
-    end
-
+    render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}  if params[:type].nil?
+    @meals = ChildStat.get_child_meals(@child_profile.id,@profile.id,params[:type])
+    render json:{:status => false, :message => "Child meals not found "} if @meals.empty?
   end
 
   def child_diapers
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-    if params[:type].nil?
-      render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}
-      return
-    end
-    @diapers = ChildStat.get_child_diapers(@child_profile.id,@parent_profile.id,params[:type])
-    if @diapers.empty?
-      render json:{:status => false, :message => "Child diapers not found "}
-    end
-  end
-
-  def get_child_parent_id(params)
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-    if params[:type].nil?
-      render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}
-      return
-    end
-    [@parent_profile.id,@child_profile.id]
+    render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}  if params[:type].nil?
+    @diapers = ChildStat.get_child_diapers(@child_profle.id,@parent_profile.id,params[:type])
+    render json:{:status => false, :message => "Child diapers not found "}  if @diapers.empty?
   end
 
   def child_full_bottles
-    child_id = get_child_parent_id(params)[0]
-    parent_id = get_child_parent_id(params)[1]
-    @child_full_bottles = ChildStat.get_child_bottle(child_id,parent_id,params[:type],1)
-    if @child_full_bottles.empty?
-      render json:{:status => false, :message => "Child full bottles not found "}
-    end
+    render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "}  if params[:type].nil?
+    @child_full_bottles = ChildStat.get_child_bottle(@child_profile.id,@profile.id,params[:type],1)
+    render json:{:status => false, :message => "Child full bottles not found "} if @child_full_bottles.empty?
   end
 
   def child_half_bottles
-    child_id = get_child_parent_id(params)[0]
-    parent_id = get_child_parent_id(params)[1]
-    @child_half_bottles = ChildStat.get_child_bottle(@child_profile.id,@parent_profile.id,params[:type],2)
-    if @child_half_bottles.empty?
-      render json:{:status => false, :message => "Child half bottles not found "}
-    end
+    render json:{:status => false, :message => "Please specify type: weekly, monthly in parameters "} if params[:type].nil?
+    @child_half_bottles = ChildStat.get_child_bottle(@child_profile.id,@profile.id,params[:type],2)
+    render json:{:status => false, :message => "Child half bottles not found "} if @child_half_bottles.empty?
   end
 
-
   def create
-    @parent_profile = ParentProfile.find(params[:profile_id])
-    begin
-      @child_profile = @parent_profile.child_profiles.find(params[:child_id])
-      raise  ActiveRecord::RecordNotFound if @child_profile.nil?
-      rescue ActiveRecord::RecordNotFound
-        render json:{:status => false, :message => "Unable to find child profile on cloud"}
+    params[:child_stat] = params[:child_stat].merge(:vaccine_id => params[:vaccine])
+    params[:child_stat] = params[:child_stat].merge(:parent_profile_id => @profile.id)
+    @child_stat = @child_profile.child_stats.new(params[:child_stat])
+    if @child_stat.save
+      render json:{:status=>true ,:child_state=>@child_stat.as_json }
     else
-        params[:child_stat] = params[:child_stat].merge(:vaccine_id => params[:vaccine])
-        params[:child_stat] = params[:child_stat].merge(:parent_profile_id => @parent_profile.id)
-        @child_stat = @child_profile.child_stats.new(params[:child_stat])
-
-        if @child_stat.save
-          render json:{:status=>true ,:child_state=>@child_stat.as_json }
-        else
-          render json:{:status => false, :message => @child_stat.errors.full_messages}
-        end
+      render json:{:status => false, :message => @child_stat.errors.full_messages}
     end
   end
 
   private
 
-  def find_profile
-    @profile = ParentProfile.find(params[:profile_id])
-  rescue ActiveRecord::RecordNotFound
-    render json:{:status => false, :message => "Unable to find parent profile on cloud"}
+  def find_child_profile
+    begin
+      @profile = ParentProfile.find(params[:profile_id])
+      @child_profile = @profile.child_profiles.find(params[:child_id])
+    rescue ActiveRecord::RecordNotFound
+      render json:{:status => false, :message => "Unable to find child profile on cloud"}
+      return  false
+    end
   end
+
+  #def child_profile_id
+  #  begin
+  #
+  #  rescue ActiveRecord::RecordNotFound
+  #    render json:{:status => false, :message => "Unable to find child profile on cloud"}
+  #    return
+  #  end
+  #end
 
   def verify_token
   #  authtoken = request.headers['authtoken']
