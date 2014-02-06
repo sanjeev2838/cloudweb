6a1268a76d5328d226aff84ee0242d076bef9348
Pardeep <pardeep@impingeonline.com>
2014-02-06 14:57:56 +0530

    Add dashboard designe

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class Admin::DashboardController < ApplicationController
 
   def index
+    @total_users = ParentProfile.all
+    @total_childs = ChildProfile.all
+    @total_registed_machines = Machine.where(:status=> true)
+    @total_unregisted_machines = Machine.where(:status=> false)
+
 
   end
 
