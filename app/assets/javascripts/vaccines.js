836b667a2321f9e76eb5934e722b52f85a1c8464
Pardeep <pardeep@impinge47-G31M-S2L.(none)>
2014-03-13 20:09:01 +0530

    vaccines numeric field validations

--- app/assets/javascripts/vaccines.js
+++ app/assets/javascripts/vaccines.js
@@ -5,13 +5,22 @@
 //})
 $(document).on("click","#submit_button", function(e) {
 
-
+    var intRegex = /^\d+$/;
    var count =  $("#count").val();
-    for (i=-1; i < count; i++){
+    if(!intRegex.test(count)) {
+        alert("Age Count Field must be numeric")
+        e.preventDefault()
+    }
+
+    for (i=0; i < count;){
 
       var age = $("#age"+i).val()
-
+        if(!intRegex.test(age)) {
+            alert("Age Field must be numeric")
+            e.preventDefault()
         }
+       i++
+    }
 
 
 });
