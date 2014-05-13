2f6a062f04f72205874951dc4882cb07646bf256
sanjeev <sanjeev.impinge@gmail.com>
2014-05-13 14:33:31 +0530

    Corrected code to show image on pdf

--- app/helpers/child_profiles_helper.rb
+++ app/helpers/child_profiles_helper.rb
@@ -6,12 +6,12 @@ module ChildProfilesHelper
     unless diary.log.nil?
       (pictures).each_with_index do |item, index|
         if log_str.include?("@image#{index+1}@")
-          log_str["@image#{index+1}@"] =  image_tag(item.image_url,:height => 30)
+          log_str["@image#{index+1}@"] =  wicked_pdf_image_tag(item.image_url,:height => 30)
         end
       end
       milestones.each_with_index do |milestone,index|
         if log_str.include?("$milestone#{index+1}$")
-          log_str["$milestone#{index+1}$"] =  image_tag(milestone.image_url,:height => 30)
+          log_str["$milestone#{index+1}$"] =  wicked_pdf_image_tag(milestone.image_url,:height => 30)
         end
       end
     end
