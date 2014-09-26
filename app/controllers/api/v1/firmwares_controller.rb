70aa15ddc22dfa248c24f0a6c738095c3d9a2608
Sanjeev <sanjeevimpinge@gmail.com>
2014-09-26 16:13:26 +0530

    Latest firmware download link

--- app/controllers/api/v1/firmwares_controller.rb
+++ app/controllers/api/v1/firmwares_controller.rb
@@ -3,21 +3,22 @@ class Api::V1::FirmwaresController <  Api::Default::BaseController
 
   def show
     @firmware = Firmware.all
+    @all_firmware = []
     @firmware.each do |firm|
-      @all_firmware = []
       machine = firm.serialids.split(',')
       if machine.include?(@machine.serialid.to_s)
           @all_firmware << firm
       end
-      @all_firmware
-    end
-    @binary_file = ""
-    unless @all_firmware.nil?
-     latest_firmware=@all_firmware.sort!{|a,b| a.created_at <=> b.created_at}
-     @binary_file = latest_firmware.map { |x| x[:binaryfile] }.join(',')
     end
 
-    unless @binary_file == ""
+    if !@all_firmware.blank?
+     @latest_firmware = @all_firmware.sort!{|a,b| a.created_at <=> b.created_at}.last
+    end
+
+    if @latest_firmware
+      extension = File.extname(@latest_firmware.binaryfile)
+      filename = File.basename(@latest_firmware.binaryfile, extension)
+      @binary_file = download_firmware_url(@latest_firmware, filename, extension.gsub(/\./, ''))
       render action: :show
     else
       render json:{:status => false, :message => "No firmware release found for this machine"}
--- app/views/api/v1/firmwares/show.json.jbuilder
+++ app/views/api/v1/firmwares/show.json.jbuilder
@@ -1,4 +1,3 @@
 json.set! :status ,true
-
-json.set! "binaryfile", request.protocol + request.host_with_port + "/" +@binary_file
+json.set! "binaryfile", @binary_file
 
